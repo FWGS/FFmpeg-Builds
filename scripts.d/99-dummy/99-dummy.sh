aa77413820f8fa9581dfab33fafa75f378d6ba2c
Alibek Omarov <a1ba.omarov@gmail.com>
2024-12-12 20:56:45 +0300

    Don't download unnecessary dependencies sources

---
+++ scripts.d/99-dummy/99-dummy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SCRIPT_SKIP="1"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerfinal() {
+    return 0
+}
+
+ffbuild_dockerdl() {
+    true
+}
+
+ffbuild_dockerlayer() {
+    return 0
+}
+
+ffbuild_dockerstage() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    return 0
+}
+
+ffbuild_ldexeflags() {
+    return 0
+}
